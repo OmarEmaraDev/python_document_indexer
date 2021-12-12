7458bcce80266a8ac9d663896b675e2e6fc462e9
Omar Emara <mail@OmarEmara.dev>
2021-12-12 20:40:25 +0200

    Add initial driver implementation

---
+++ indexer/__main__.py
@@ -0,0 +1,81 @@
+import os
+import sys
+from pathlib import Path
+from . tokenizer import Tokenizer
+from argparse import ArgumentParser
+from . scanner import WhiteSpaceScanner
+from . stop_list import ReutersRCV1StopList
+from . normalizer import LowerCaseNormalizer
+from . positional_index import PositionalIndex
+from . document_collection import DocumentCollection
+
+def printErrorAndExit(error, exitCode = 1):
+    print("indexer: " + error)
+    sys.exit(exitCode)
+
+def index(arguments):
+    scanner = WhiteSpaceScanner()
+    stopList = ReutersRCV1StopList()
+    normalizer = LowerCaseNormalizer()
+    tokenizer = Tokenizer(scanner, stopList, normalizer)
+    documentCollection = DocumentCollection(arguments.collection)
+    positionalIndex = PositionalIndex(tokenizer, documentCollection)
+    positionalIndex.save()
+
+def query(arguments):
+    pass
+
+def validateDisplay(arguments):
+    if not (arguments.collection / ".index").exists():
+        printErrorAndExit("No index exists. Run 'indexer index' first!")
+
+def display(arguments):
+    validateDisplay(arguments)
+    positionalIndex = PositionalIndex.load(arguments.collection)
+    print(positionalIndex)
+
+def parseArguments():
+    parser = ArgumentParser(prog = "indexer")
+    subParsers = parser.add_subparsers(title = "Actions", required = True,
+        description = "The action to apply over the input document collection.",
+        metavar = "ACTION")
+
+    collectionHelp = "The path of the directory containing the document collection."
+
+    indexHelp = "Compute the index for collection."
+    indexParser = subParsers.add_parser("index",
+        help = indexHelp, description = indexHelp)
+    indexParser.add_argument("collection", type = Path, nargs = "?",
+        metavar = "COLLECTION", default = Path.cwd(), help = collectionHelp)
+    indexParser.set_defaults(handler = index)
+
+    queryHelp = "Query the documents that includes the input phrase."
+    queryParser = subParsers.add_parser("query",
+        help = queryHelp, description = queryHelp)
+    queryParser.add_argument("phrase", metavar = "PHRASE",
+        help = "The phrase to query for.")
+    queryParser.add_argument("collection", type = Path, nargs = "?",
+        metavar = "COLLECTION", default = Path.cwd(), help = collectionHelp)
+    queryParser.set_defaults(handler = query)
+
+    displayHelp = "Display the positional index for collection."
+    displayParser = subParsers.add_parser("display",
+        help = displayHelp, description = displayHelp)
+    displayParser.add_argument("collection", type = Path, nargs = "?",
+        metavar = "COLLECTION", default = Path.cwd(), help = collectionHelp)
+    displayParser.set_defaults(handler = display)
+
+    return parser.parse_args()
+
+def validateCommonArguments(arguments):
+    if not arguments.collection.is_dir():
+        printErrorAndExit("Collection should be a path to a directory.")
+
+def main():
+    arguments = parseArguments()
+    validateCommonArguments(arguments)
+    arguments.handler(arguments)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
