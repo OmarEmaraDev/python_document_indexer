37cd10440ba5f4bdc5f7c0638d6384104a20b448
Ahmed Ezzat Nasr <41273735+0xOrigin@users.noreply.github.com>
2021-12-16 11:45:45 +0200

    Initial phrase query implementation (#6)

--- indexer/positional_index.py
+++ indexer/positional_index.py
@@ -1,4 +1,5 @@
 import pickle
+from . utilities import pairwise
 from io import StringIO
 from . tokenizer import Tokenizer
 from collections import OrderedDict
@@ -92,12 +93,48 @@ class PositionalIndex:
     #######
     # Query
     #######
-
+    
     def phraseQuery(self, phrase):
         tokens = self.tokenizer(StringIO(phrase))
         postingsLists = [self.dictionary[token] for token in tokens]
-        raise NotImplementedError
+        
+        biWordMatches = (self.positionalIntersect(x, y) for x, y in pairwise(postingsLists))
+        return set.intersection(*biWordMatches)
 
+
+    def positionalIntersect(self, posting1, posting2):
+        answer = set()
+        k = 1
+        p1 = posting1.postings
+        p2 = posting2.postings
+        postingIndex = targetPostingIndex = 0
+
+        while postingIndex != len(p1) and targetPostingIndex != len(p2):
+
+            if p1[postingIndex].document.id == p2[targetPostingIndex].document.id:
+                pp1 = p1[postingIndex].positions
+                pp2 = p2[targetPostingIndex].positions
+                positionIndex = targetPositionIndex = 0
+
+                while positionIndex != len(pp1) and targetPositionIndex != len(pp2):
+                    if pp2[targetPositionIndex] - pp1[positionIndex] == k:
+                        answer.add(p1[postingIndex].document.id)
+                        positionIndex += 1
+                        targetPositionIndex += 1
+                    elif pp2[targetPositionIndex] - pp1[positionIndex] > k:
+                        positionIndex += 1
+                    else:
+                        targetPositionIndex += 1
+
+                postingIndex += 1
+                targetPostingIndex += 1
+
+            elif p1[postingIndex].document.id > p2[targetPostingIndex].document.id:
+                targetPostingIndex += 1
+            else:
+                postingIndex += 1
+        return answer
+        
     ###########
     # Load/Save
     ###########
